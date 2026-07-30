28d5cff03eced3e9034a91a09de0c3cc7d6dbbbc
abergman <abergman@student.42.fr>
2026-07-30 12:45:14 +0200

    control(2): flowing LED

--- Flowing_LED/Flowing_LED.ino
+++ Flowing_LED/Flowing_LED.ino
@@ -1,31 +1,28 @@
-const int ledCount = 14;
-
-int ledPins[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13};
+int ledCount = 14;
+int inxedLed[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13};
 
 void setup() {
     for (int i = 0; i < ledCount; i++) {
-        pinMode(ledPins[i], OUTPUT);
+        pinMode(i, OUTPUT);
     }
 }
 
 void loop() {
     for (int i = 0; i < ledCount; i++) {
-        barGraphDisplay(i);
+        flowingLed(i);
     }
-
-    for (int i = ledCount - 1; i >= 0; i--) {
-        barGraphDisplay(i);
+    for (int i = ledCount - 1; i > 0; i--) {
+        flowingLed(i);
     }
 }
 
-void barGraphDisplay(int ledOn) {
-    for (int i = 0; i< ledCount; i++) {
-        if (i == ledOn) {
-            digitalWrite(ledPins[i], HIGH);
-        }
-        else {
-            digitalWrite(ledPins[i], LOW);
+void flowingLed(int change) {
+    for(int i = 0; i < ledCount; i++) {
+        if (change == i) {
+            digitalWrite(i, HIGH);
+        } else {
+            digitalWrite(i, LOW);
         }
     }
-    delay(100);
+    delay(50);
 }
